ac610d0bc454866ed4a71d6b32dea017d57f4811
acamalvarez <acam@outlook.com>
2020-05-27 12:30:40 -0500

    update doc

--- calculate_pH.py
+++ calculate_pH.py
@@ -9,14 +9,29 @@ class MyWindow(QMainWindow):
 		uic.loadUi('calculate_pH.ui', self)
 		self.buttonCalculate.clicked.connect(self.calculate_pH)
 
-	def calculate_pH(self,):
+	def calculate_pH(self):
+		'''
+		Calculates the pH of a solution. It takes the value of the concentration in the label concentration
+
+		-- Update
+		The value of the pH in the label solution 
+		'''
 		
+		# Gets the value from concentration and converts into a float
 		self.concentration = float(self.inputValue.text())
+		# calculates the pH
 		self.pH = -np.round(np.log10(self.concentration), 2)
+		# Set the label solution to pH
 		self.labelSolution.setText(str(self.pH))
+
+		# updates the value of the label solution
 		self.update()
 
 	def update(self):
+		'''
+		Updates the values of the labels
+		'''
+		# updates label solution
 		self.labelSolution.adjustSize()
 		
 		
